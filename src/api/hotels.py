59ce4121843f1b49192ba8aed03d5560dd24b063
storlay <dmitriy.site1@gmail.com>
2025-05-29 16:03:28 -0300

    Implement sqlalchemy to api

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 from fastapi import Body
 from fastapi import Query
+from sqlalchemy import insert
+from sqlalchemy import select
 
 from src.api.dependecies import PaginationDep
+from src.db.database import async_session
+from src.models.hotels import Hotels
 from src.schemas.hotels import CreateHotelSchema
 from src.schemas.hotels import PartialUpdateHotelSchema
 
@@ -14,14 +18,29 @@ router = APIRouter(
 
 
 @router.get("/")
-def get_hotels(
+async def get_hotels(
     pagination: PaginationDep,
-    name: str = Query(
+    title: str = Query(
         None,
-        description="Hotel's name",
+        description="Hotel's title",
     ),
+    location: str = Query(
+            None,
+            description="Hotel's location",
+        ),
 ):
-    pass
+    limit = pagination.per_page
+    offset = pagination.per_page * (pagination.page - 1)
+    query = select(Hotels)
+    if title:
+        query = query.filter(Hotels.title.icontains(title))
+    if location:
+        query = query.filter(Hotels.location.icontains(location))
+    query = query.limit(limit).offset(offset)
+    async with async_session() as session:
+        result = await session.execute(query)
+    hotels = result.scalars().all()
+    return hotels
 
 
 @router.delete("/{hotel_id}")
@@ -32,10 +51,23 @@ def delete_hotel(
 
 
 @router.post("/")
-def add_hotel(
-    data: CreateHotelSchema,
+async def add_hotel(
+    data: CreateHotelSchema = Body(
+        openapi_examples={
+            "1": {
+                "summary": "Dubai",
+                "value": {
+                    "title": "Dubai 5 stars",
+                    "location": "Sheikh street, 1",
+                },
+            }
+        }
+    ),
 ):
-    pass
+    async with async_session() as session:
+        add_hotel_stmt = insert(Hotels).values(**data.model_dump())
+        await session.execute(add_hotel_stmt)
+        await session.commit()
 
 
 @router.put("/{hotel_id}")
