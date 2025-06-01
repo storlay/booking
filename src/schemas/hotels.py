d54703c914fea0a87893f46256622b1dbe16a1f1
storlay <dmitriy.site1@gmail.com>
2025-06-01 17:18:09 -0300

    Update hotels schemas

--- src/schemas/hotels.py
+++ src/schemas/hotels.py
@@ -1,10 +1,19 @@
 from pydantic import BaseModel
+from pydantic import ConfigDict
 from pydantic import Field
 
 
 class CreateOrUpdateHotelSchema(BaseModel):
     title: str
     location: str
+
+
+class HotelSchema(CreateOrUpdateHotelSchema):
+    id: int
+
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
 
 
 class PartialUpdateHotelSchema(BaseModel):
