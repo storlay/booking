06c1c69f3c5636e8357dfe706ab3ee990865b840
storlay <dmitriy.site1@gmail.com>
2025-06-02 12:06:33 -0300

    Implement user login endpoint

--- src/api/v1/auth.py
+++ src/api/v1/auth.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 from fastapi import HTTPException
+from fastapi import Response
 from fastapi import status
 from sqlalchemy.exc import IntegrityError
 
 from src.db.database import async_session
 from src.repositories.users import UsersRepository
-from src.schemas.users import RegisterUserSchema
-from src.utils.auth import hash_password
+from src.schemas.users import UserAuthSchema
+from src.services.auth import AuthService
 
 
 router = APIRouter(
@@ -17,9 +18,9 @@ router = APIRouter(
 
 @router.post("/register")
 async def register_user(
-    data: RegisterUserSchema,
-) -> None:
-    data.password = hash_password(data.password)
+    data: UserAuthSchema,
+):
+    data.password = AuthService().hash_password(data.password)
     async with async_session() as session:
         try:
             await UsersRepository(session).add(
@@ -31,3 +32,29 @@ async def register_user(
                 status.HTTP_400_BAD_REQUEST,
                 detail=f"User with email {data.email!r} already exists.",
             )
+    return {"status": "OK"}
+
+
+@router.post("/login")
+async def login_user(
+    data: UserAuthSchema,
+    response: Response,
+):
+    async with async_session() as session:
+        user = await UsersRepository(session).get_one_with_password(
+            email=data.email,
+        )
+    if not user or not AuthService().verify_password(data.password, user.password):
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password.",
+        )
+    access_token = AuthService().create_access_token({"user_id": user.id})
+    response.set_cookie("access_token", access_token)
+    return {
+        "status": "OK",
+        "data": {
+            "access": access_token,
+        },
+    }
+
