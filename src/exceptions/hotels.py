5d480d95e1a785d67de748580b0523ee159bd223
storlay <dmitriy.site1@gmail.com>
2025-06-05 17:39:12 -0300

    Update Hotels exceptions

--- src/exceptions/hotels.py
+++ src/exceptions/hotels.py
@@ -6,8 +6,3 @@ from src.exceptions.base import BaseHTTPException
 class HotelNotFoundException(BaseHTTPException):
     status_code = status.HTTP_404_NOT_FOUND
     detail = "Hotel not found"
-
-
-class MultipleHotelsFoundException(BaseHTTPException):
-    status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
-    detail = "Found many hotels, expected one"
