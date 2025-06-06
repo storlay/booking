c67ad47a273a31fc2a6cd04abb6d83fad1a4f2b2
storlay <dmitriy.site1@gmail.com>
2025-06-06 15:10:18 -0300

    Create base field for positive decimal

--- src/schemas/base.py
+++ src/schemas/base.py
@@ -1,4 +1,11 @@
+from decimal import Decimal
+from typing import Annotated
+
+from annotated_types import Ge
+from annotated_types import Le
 from pydantic import BaseModel
+
+from src.config.config import settings
 
 
 class BaseSuccessResponseSchema(BaseModel):
@@ -7,3 +14,10 @@ class BaseSuccessResponseSchema(BaseModel):
 
 class BaseHTTPExceptionSchema(BaseModel):
     detail: str
+
+
+PositiveDecimal = Annotated[
+    Decimal,
+    Ge(0),
+    Le(settings.models.MAX_DECIMAL_VALUE),
+]
