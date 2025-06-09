bff2ecd18d06ed7e655131fb79b00b120c46ef43
storlay <dmitriy.site1@gmail.com>
2025-06-09 14:04:48 -0300

    Update Bookings api

--- src/api/v1/bookings.py
+++ src/api/v1/bookings.py
@@ -4,6 +4,7 @@ from sqlalchemy.exc import NoResultFound
 
 from src.api.dependecies import CurrentUserDep
 from src.api.dependecies import DbTransactionDep
+from src.exceptions.auth import InvalidAuthTokenException
 from src.exceptions.bookings import InvalidRoomIdForBookingException
 from src.schemas.base import BaseHTTPExceptionSchema
 from src.schemas.bookings import BookingCreateRequestSchema
@@ -17,6 +18,29 @@ router = APIRouter(
 )
 
 
+@router.get(
+    "/",
+    response_model=list[BookingSchema],
+    status_code=status.HTTP_200_OK,
+)
+async def get_all_bookings(
+    transaction: DbTransactionDep,
+) -> list[BookingSchema]:
+    return await transaction.bookings.get_all()
+
+
+@router.get(
+    "/me",
+    response_model=list[BookingSchema],
+    status_code=status.HTTP_200_OK,
+)
+async def get_me_bookings(
+    user: CurrentUserDep,
+    transaction: DbTransactionDep,
+) -> list[BookingSchema]:
+    return await transaction.bookings.get_filtered(user_id=user.id)
+
+
 @router.post(
     "/",
     response_model=BookingSchema,
@@ -26,13 +50,17 @@ router = APIRouter(
             "model": BaseHTTPExceptionSchema,
             "description": InvalidRoomIdForBookingException.detail,
         },
+        InvalidAuthTokenException.status_code: {
+            "model": BaseHTTPExceptionSchema,
+            "description": InvalidAuthTokenException.detail,
+        },
     },
 )
 async def create_booking(
     user: CurrentUserDep,
     transaction: DbTransactionDep,
     data: BookingCreateRequestSchema,
-):
+) -> BookingSchema:
     try:
         room = await transaction.rooms.get_one(id=data.room_id)
     except NoResultFound:
