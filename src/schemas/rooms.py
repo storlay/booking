2c10abffe75c251ab011e82860f54d731ab3a78f
storlay <dmitriy.site1@gmail.com>
2025-06-10 14:00:29 -0300

    Update Rooms schemas

--- src/schemas/rooms.py
+++ src/schemas/rooms.py
@@ -1,7 +1,6 @@
 from datetime import date
 from typing import Annotated
 
-from annotated_types import Ge
 from annotated_types import MaxLen
 from annotated_types import MinLen
 from pydantic import BaseModel
@@ -9,6 +8,7 @@ from pydantic import ConfigDict
 
 from src.schemas.base import IntegerId
 from src.schemas.base import PositiveDecimal
+from src.schemas.base import PositiveInteger
 
 
 class RoomCreateRequestSchema(BaseModel):
@@ -17,19 +17,26 @@ class RoomCreateRequestSchema(BaseModel):
         MaxLen(100),
     ]
     price: PositiveDecimal
-    quantity: Annotated[
-        int,
-        Ge(0),
-    ]
+    quantity: PositiveInteger
+    facilities_ids: list[IntegerId]  | None = None
     description: Annotated[
         str | None,
         MinLen(10),
     ] = None
-    facilities_ids: list[IntegerId]
 
 
-class RoomCreateSchema(RoomCreateRequestSchema):
+class RoomCreateSchema(BaseModel):
     hotel_id: IntegerId
+    title: Annotated[
+        str,
+        MaxLen(100),
+    ]
+    price: PositiveDecimal
+    quantity: PositiveInteger
+    description: Annotated[
+        str | None,
+        MinLen(10),
+    ]
 
 
 class RoomSchema(RoomCreateSchema):
@@ -54,10 +61,8 @@ class RoomPartiallyUpdateSchema(BaseModel):
         MinLen(10),
     ] = None
     price: PositiveDecimal = None
-    quantity: Annotated[
-        int | None,
-        Ge(0),
-    ] = None
+    quantity: PositiveInteger | None = None
+    facilities_ids: list[IntegerId] = None
 
 
 class RoomsQueryParamsSchema(BaseModel):
