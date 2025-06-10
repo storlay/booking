9386b25f56de7276710b363d0c8dc9b08988ec8b
storlay <dmitriy.site1@gmail.com>
2025-06-10 12:29:06 -0300

    Add Facilities exceptions

---
+++ src/exceptions/facilities.py
@@ -0,0 +1,8 @@
+from fastapi import status
+
+from src.exceptions.base import BaseHTTPException
+
+
+class FacilityAlreadyExistsException(BaseHTTPException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    detail = "Facility with this title already exists"
