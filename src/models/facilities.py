89ad15daab0fac7305c1ecb622f45070dcb4a032
storlay <dmitriy.site1@gmail.com>
2025-06-11 10:19:16 -0300

    Update rooms m2m

--- src/models/facilities.py
+++ src/models/facilities.py
@@ -1,17 +1,29 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 from src.db import Base
 from src.db.mixins import IntPkModelMixin
+
+
+if TYPE_CHECKING:
+    from src.models.rooms import Rooms
 
 
 class Facilities(Base, IntPkModelMixin):
     title: Mapped[str] = mapped_column(
         String(100),
         unique=True,
+    )
+
+    rooms: Mapped[list["Rooms"]] = relationship(
+        back_populates="facilities",
+        secondary="rooms_facilities",
     )
 
     def __repr__(self):
--- src/models/rooms.py
+++ src/models/rooms.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from typing import TYPE_CHECKING
 
 from sqlalchemy import DECIMAL
 from sqlalchemy import CheckConstraint
@@ -7,10 +8,15 @@ from sqlalchemy import String
 from sqlalchemy import Text
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 from src.config import settings
 from src.db import Base
 from src.db.mixins import IntPkModelMixin
+
+
+if TYPE_CHECKING:
+    from src.models.facilities import Facilities
 
 
 class Rooms(Base, IntPkModelMixin):
@@ -30,6 +36,11 @@ class Rooms(Base, IntPkModelMixin):
         ),
     )
     quantity: Mapped[int]
+
+    facilities: Mapped[list["Facilities"]] = relationship(
+        back_populates="rooms",
+        secondary="rooms_facilities",
+    )
 
     def __repr__(self):
         return f"<Room id={self.id!r}, title={self.title!r}>"
