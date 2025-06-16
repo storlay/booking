02f9f9a57824f5c7229a6cdfe867e918368718d4
storlay <dmitriy.site1@gmail.com>
2025-06-16 13:31:48 -0300

    Add null pool db engine

--- src/db/database.py
+++ src/db/database.py
@@ -1,5 +1,6 @@
 import re
 
+from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import DeclarativeBase
@@ -11,8 +12,17 @@ from src.config import settings
 async_engine = create_async_engine(
     url=settings.db.URL,
 )
+async_engine_null_pull = create_async_engine(
+    url=settings.db.URL,
+    poolclass=NullPool,
+)
+
 async_session = async_sessionmaker(
     async_engine,
+    expire_on_commit=False,
+)
+async_session_null_pool = async_sessionmaker(
+    async_engine_null_pull,
     expire_on_commit=False,
 )
 
