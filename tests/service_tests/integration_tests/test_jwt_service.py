ccb53e8d2b78c5968660aa487a7f3073ce7fad0b
storlay <dmitriy.site1@gmail.com>
2025-06-18 13:38:55 -0300

    Add base tests

---
+++ tests/service_tests/integration_tests/test_jwt_service.py
@@ -0,0 +1,12 @@
+from src.services.jwt import JWTService
+
+
+def test_encode_and_decode_token():
+    user_id = 1
+    token = JWTService.create_access_token_for_user(user_id)
+    assert token
+    assert isinstance(token, str)
+    decoded_payload = JWTService.decode(token)
+    assert decoded_payload.get("sub")
+    assert int(decoded_payload["sub"]) == user_id
+
