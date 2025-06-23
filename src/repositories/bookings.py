883acc00f2472ec6f5d87d6249cce3a06e7dee35
storlay <dmitriy.site1@gmail.com>
2025-06-23 16:04:34 -0300

    Update Booking repository

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -3,9 +3,12 @@ from datetime import date
 from pydantic import BaseModel
 from sqlalchemy import select
 
+from src.exceptions.repository.bookings import RoomUnavailableRepoException
 from src.models.bookings import Bookings
 from src.repositories.base import BaseRepository
 from src.repositories.mappers.bookings import BookingsDataMapper
+from src.repositories.utils import rooms_ids_for_booking
+from src.schemas.bookings import BookingCreateSchema
 
 
 class BookingsRepository(BaseRepository):
@@ -24,3 +27,17 @@ class BookingsRepository(BaseRepository):
             for model in result.scalars().all()
         ]
         # fmt: on
+
+    async def add_booking(
+        self,
+        data: BookingCreateSchema,
+    ):
+        rooms_ids_query = rooms_ids_for_booking(
+            data.date_from,
+            data.date_to,
+        )
+        result = await self.session.execute(rooms_ids_query)
+        available_rooms_ids = result.scalars().all()
+        if data.room_id not in available_rooms_ids:
+            raise RoomUnavailableRepoException
+        return await self.add(data)
